208238e93409990db7a0f7d4e9cf0db16d50b860
jshuang0520 <s9140911@yahoo.com.tw>
2025-03-22 01:17:25 +0800

    chore: read data

---
+++ notebooks/untitled.py
@@ -0,0 +1,14 @@
+class info():
+
+
+class infoGetter(info):
+    def get_language():
+        """
+        user's preferred language
+        """
+        pass
+    def get_food_preference():
+        pass
+    def get_distance():
+        pass
+        
