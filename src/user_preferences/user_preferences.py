540c202a4c4b7c320025746fada7c269cd58d215
jshuang0520 <s9140911@yahoo.com.tw>
2025-04-07 19:10:22 -0400

    chore: enhance the import

--- src/user_preferences/user_preferences.py
+++ src/user_preferences/user_preferences.py
@@ -2,10 +2,10 @@ import sys
 import os
 from datetime import datetime, timedelta
 
-# Insert the project root into sys.path.
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
+# # Insert the project root into sys.path.
+# project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+# if project_root not in sys.path:
+#     sys.path.insert(0, project_root)
 
 from src.utilities import load_config  # from src/utilities/__init__.py
 
